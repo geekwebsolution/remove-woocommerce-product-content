16cb0d49408a4e1b27b76b8d1f2743cad95d058a
kp2609 <kaushalgeek435@gmail.com>
2025-01-06 17:19:32 +0530

    added description comment in all the functions

--- functions.php
+++ functions.php
@@ -1,26 +1,89 @@
 <?php 
 if(!defined('ABSPATH')) exit;
 
-// Get single page option
-function get_option_rwpc_wc_single()
-{
-	return get_option('rwpc_hide_single_wcpage_hook');
-}
-// Get product page option
-function get_option_rwpc_wc_product()
-{
-	return get_option('rwpc_hide_product_wcpage_hook');
-}
-
-// Success message
-function  success_option_msg_rwpc($msg)
-{	
-	return ' <div class="notice notice-success rwpc-success-msg is-dismissible"><p>'. $msg . '</p></div>';		
+/**
+ * Retrieves the option value for hiding single WooCommerce page content.
+ *
+ * This function retrieves the value of the 'rwpc_hide_single_wcpage_hook' option, which is used to determine
+ * whether the content of single WooCommerce pages should be hidden or not.
+ *
+ * @since 1.0.0
+ *
+ * @return mixed The value of the 'rwpc_hide_single_wcpage_hook' option. If the option is not set, returns false.
+ */
+function get_option_rwpc_wc_single(){
+    return get_option('rwpc_hide_single_wcpage_hook');
 }
 
-// Error message
-function  failure_option_msg_rwpc($msg)
-{
-	return '<div class="notice notice-error rwpc-error-msg is-dismissible"><p>' . $msg . '</p></div>';		
+/**
+ * Retrieves the option value for hiding product WooCommerce page content.
+ *
+ * This function retrieves the value of the 'rwpc_hide_product_wcpage_hook' option, which is used to determine
+ * whether the content of single product WooCommerce pages should be hidden or not.
+ *
+ * @since 1.0.0
+ *
+ * @return mixed The value of the 'rwpc_hide_product_wcpage_hook' option. If the option is not set, returns false.
+ */
+function get_option_rwpc_wc_product(){
+    return get_option('rwpc_hide_product_wcpage_hook');
 }
-?>
+
+
+/**
+ * Displays a success message in the admin dashboard.
+ *
+ * This function generates a success message to be displayed in the admin dashboard.
+ * The message is wrapped in HTML div tags with specific classes for styling.
+ *
+ * @since 1.0.0
+ *
+ * @param string $msg The success message to be displayed.
+ *
+ * @return string The success message wrapped in HTML div tags.
+ */
+function  success_option_msg_rwpc($msg){	
+    return ' <div class="notice notice-success rwpc-success-msg is-dismissible"><p>'. $msg . '</p></div>';
+}
+
+
+/**
+ * Displays an error message in the admin dashboard.
+ *
+ * This function generates an error message to be displayed in the admin dashboard.
+ * The message is wrapped in HTML div tags with specific classes for styling.
+ *
+ * @since 1.0.0
+ *
+ * @param string $msg The error message to be displayed.
+ *
+ * @return string The error message wrapped in HTML div tags.
+ */
+function  failure_option_msg_rwpc($msg){
+    return '<div class="notice notice-error rwpc-error-msg is-dismissible"><p>' . $msg . '</p></div>';
+}
+
+
+if ( ! function_exists( 'rwpc_install_woocommerce_admin_notice' ) ) {
+	/**
+	 * Displays an admin notice when WooCommerce is not active.
+	*
+	* This function is called when the plugin is activated and WooCommerce is not installed.
+	* It displays an error message in the admin dashboard, informing the user that the plugin is enabled but not effective.
+	*
+	* @since 1.0.0
+	*
+	* @return void
+	*/
+	function rwpc_install_woocommerce_admin_notice() { ?>
+		<div class="error">
+			<p>
+				<?php
+				// translators: %s is the plugin name.
+				echo esc_html__( sprintf( '%s is enabled but not effective. It requires WooCommerce in order to work.', 'Remove Woocommerce Product Content' ), 'remove-woocommerce-product-content' );
+				?>
+			</p>
+		</div>
+		<?php
+	}
+}
--- remove-woocommerce-product-content.php
+++ remove-woocommerce-product-content.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Remove Woocommerce Product Content
-Description: Hide product content from  wooocommerce Single product page, Shop page and category page
+Description: Hide product content from  woocommerce Single product page, Shop page and category page
 Author: Geek Code Lab
 Version: 2.3.0
 WC tested up to: 8.6.1
@@ -12,116 +12,170 @@ if(!defined('ABSPATH')) exit;
 
 define("RWPC_BUILD","2.3.0");
 
-if(!defined("RWPC_PLUGIN_DIR_PATH"))
-	define("RWPC_PLUGIN_DIR_PATH",plugin_dir_path(__FILE__));
+if(!defined("RWPC_PLUGIN_DIR_PATH")) define("RWPC_PLUGIN_DIR_PATH",plugin_dir_path(__FILE__));
 	
-if(!defined("RWPC_PLUGIN_URL"))
-	define("RWPC_PLUGIN_URL", plugins_url().'/'.basename(dirname(__FILE__)));
+if(!defined("RWPC_PLUGIN_URL")) define("RWPC_PLUGIN_URL", plugins_url().'/'.basename(dirname(__FILE__)));
 
-if (!defined("RWPC_PLUGIN_BASENAME"))
-	define("RWPC_PLUGIN_BASENAME", plugin_basename(__FILE__));
+if (!defined("RWPC_PLUGIN_BASENAME")) define("RWPC_PLUGIN_BASENAME", plugin_basename(__FILE__));
 
-if (!defined("RWPC_PLUGIN_DIR"))
-	define("RWPC_PLUGIN_DIR", plugin_basename(__DIR__));
-	
+if (!defined("RWPC_PLUGIN_DIR")) define("RWPC_PLUGIN_DIR", plugin_basename(__DIR__));
 
 require_once( RWPC_PLUGIN_DIR_PATH .'functions.php');
-
 require(RWPC_PLUGIN_DIR_PATH . 'updater/updater.php');
 
-add_action('admin_menu', 'rwpc_admin_option' );
-
-add_action('admin_enqueue_scripts','rwpc_enqueue_styles');
-
-add_action('admin_init', 'rwpc_registerSettings');
-
-
-
-function rwpc_plugin_add_settings_link( $links ) { 
-	$support_link = '<a href="https://geekcodelab.com/contact/"  target="_blank" >' . __( 'Support', 'remove-woocommerce-product-content' ) . '</a>';
-	array_unshift( $links, $support_link );
-	
-	$pro_link = '<a href="https://geekcodelab.com/wordpress-plugins/remove-woocommerce-product-content-pro/"  target="_blank" style="color:#46b450;font-weight: 600;">' . __( 'Premium Upgrade', 'remove-woocommerce-product-content' ) . '</a>'; 
-	array_unshift( $links, $pro_link );	
-
-	$settings_link = '<a href="'.admin_url('').'admin.php?page=rwpc-remove-woocommerce-product-content">' . __( 'Settings', 'remove-woocommerce-product-content' ) . '</a>';
-	array_unshift( $links, $settings_link );
-
-	return $links;	
-}
-$plugin = plugin_basename( __FILE__ );
-add_filter( "plugin_action_links_$plugin", 'rwpc_plugin_add_settings_link');
-
-
-// Register activation hook
-register_activation_hook( __FILE__, 'rwpc_plugin_active_single_product_page' );
-function rwpc_plugin_active_single_product_page() {
-	rwpc_updater_activate();
-	if (is_plugin_active( 'remove-woocommerce-product-content-pro/remove-woocommerce-product-content-pro.php' ) ) {		
-		deactivate_plugins('remove-woocommerce-product-content-pro/remove-woocommerce-product-content-pro.php');
-   	}
-}
 add_action('upgrader_process_complete', 'rwpc_updater_activate'); // remove  transient  on plugin  update
 
-/** Trigger an admin notice if WooCommerce is not installed.*/
-if ( ! function_exists( 'rwpc_install_woocommerce_admin_notice' ) ) {
-	function rwpc_install_woocommerce_admin_notice() { ?>
-		<div class="error">
-			<p>
-				<?php
-				// translators: %s is the plugin name.
-				echo esc_html__( sprintf( '%s is enabled but not effective. It requires WooCommerce in order to work.', 'Remove Woocommerce Product Content' ), 'remove-woocommerce-product-content' );
-				?>
-			</p>
-		</div>
-		<?php
-	}
+/**
+ * Adds links to the plugin's settings page and support resources in the plugin action links.
+ *
+ * This function is hooked to the 'plugin_action_links_$plugin' filter and adds links to the plugin's settings page,
+ * support resources, and a premium upgrade link. The links are added to the plugin's action links in the WordPress admin area.
+ *
+ * @since 1.0.0
+ *
+ * @param array $links An array of plugin action links.
+ *
+ * @return array The modified array of plugin action links.
+ */
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'rwpc_plugin_add_settings_link');
+function rwpc_plugin_add_settings_link( $links ) { 
+    $support_link = '<a href="https://geekcodelab.com/contact/"  target="_blank" >' . __( 'Support', 'remove-woocommerce-product-content' ) . '</a>';
+    array_unshift( $links, $support_link );
+
+    $pro_link = '<a href="https://geekcodelab.com/wordpress-plugins/remove-woocommerce-product-content-pro/"  target="_blank" style="color:#46b450;font-weight: 600;">' . __( 'Premium Upgrade', 'remove-woocommerce-product-content' ) . '</a>'; 
+    array_unshift( $links, $pro_link );
+
+    $settings_link = '<a href="'.admin_url('').'admin.php?page=rwpc-remove-woocommerce-product-content">' . __( 'Settings', 'remove-woocommerce-product-content' ) . '</a>';
+    array_unshift( $links, $settings_link );
+
+    return $links;
 }
+
+
+/**
+ * Activates the Remove Woocommerce Product Content plugin and deactivates the premium version if it's active.
+ *
+ * This function is hooked to the 'activation_hook' for the plugin file. It calls the updater activation function,
+ * checks if the premium version of the plugin is active, and deactivates it if it is.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+register_activation_hook( __FILE__, 'rwpc_plugin_active_single_product_page' );
+function rwpc_plugin_active_single_product_page() {
+    rwpc_updater_activate();
+    if (is_plugin_active( 'remove-woocommerce-product-content-pro/remove-woocommerce-product-content-pro.php' ) ) {		
+        deactivate_plugins('remove-woocommerce-product-content-pro/remove-woocommerce-product-content-pro.php');
+       }
+}
+
+/**
+ * Initializes the Remove Woocommerce Product Content plugin and checks for WooCommerce installation.
+ *
+ * This function is hooked to the 'plugins_loaded' action and checks if WooCommerce is installed.
+ * If WooCommerce is not installed, it adds an admin notice to prompt the user to install WooCommerce.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+add_action( 'plugins_loaded', 'rwpc_woocommerce_constructor' );
 function rwpc_woocommerce_constructor() {
     // Check WooCommerce installation
-	if ( ! function_exists( 'WC' ) ) {
-		add_action( 'admin_notices', 'rwpc_install_woocommerce_admin_notice' );
-		return;
-	}
+    if ( ! function_exists( 'WC' ) ) {
+        add_action( 'admin_notices', 'rwpc_install_woocommerce_admin_notice' );
+        return;
+    }
 }
-add_action( 'plugins_loaded', 'rwpc_woocommerce_constructor' );
 
+
+/**
+ * Registers plugin settings for hiding WooCommerce hooks on single product and product category pages.
+ *
+ * This function retrieves the plugin's name from the plugin data and registers two settings:
+ * - rwpc_hide_single_wcpage_hook: Stores the selected hooks to hide on the single product page.
+ * - rwpc_hide_product_wcpage_hook: Stores the selected hooks to hide on the product category page.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+add_action('admin_init', 'rwpc_registerSettings');
 function rwpc_registerSettings() {
-	$plugin_data = get_plugin_data( __FILE__ );
-	$plugin_name = $plugin_data['Name'];
-	register_setting( $plugin_name, 'rwpc_hide_single_wcpage_hook');
-	register_setting( $plugin_name, 'rwpc_hide_product_wcpage_hook');
+    $plugin_data = get_plugin_data( __FILE__ );
+    $plugin_name = $plugin_data['Name'];
+    register_setting( $plugin_name, 'rwpc_hide_single_wcpage_hook');
+    register_setting( $plugin_name, 'rwpc_hide_product_wcpage_hook');
 }
 
+
+/**
+ * Adds a submenu page under the WooCommerce menu in the WordPress admin area.
+ * This page is used to manage the plugin's settings and options.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+add_action('admin_menu', 'rwpc_admin_option' );
 function rwpc_admin_option() {
     add_submenu_page(
-		'woocommerce',
-        'Remove Product Content',
-		'Remove Product Content',
-        'manage_options',
-        'rwpc-remove-woocommerce-product-content',
-		'rwpc_option_menu'
-	);
+        'woocommerce', // The parent menu slug.
+        'Remove Product Content', // The page title.
+        'Remove Product Content', // The menu title.
+        'manage_options', // The capability required to access this menu.
+        'rwpc-remove-woocommerce-product-content', // The menu slug.
+        'rwpc_option_menu' // The function to call to render the page content.
+    );
 }
 
-function rwpc_option_menu()
-{
-	include( RWPC_PLUGIN_DIR_PATH . 'options.php');	
+
+/**
+ * This function is used to render the plugin's settings and options page in the WordPress admin area.
+ * It includes the 'options.php' file from the plugin's directory.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function rwpc_option_menu(){
+    include( RWPC_PLUGIN_DIR_PATH . 'options.php');
 }
 
+
+/**
+ * Enqueues styles and scripts for the Remove Woocommerce Product Content plugin.
+ *
+ * This function is hooked to the 'admin_enqueue_scripts' action and is responsible for
+ * enqueuing the necessary CSS and JavaScript files for the plugin's settings page.
+ *
+ * @since 1.0.0
+ *
+ * @param string $hook The current admin page hook.
+ *
+ * @return void
+ */
+add_action('admin_enqueue_scripts','rwpc_enqueue_styles');
 function rwpc_enqueue_styles($hook) {
-	if($hook == 'woocommerce_page_rwpc-remove-woocommerce-product-content') {
-		//STYLES
-		wp_enqueue_style("rwpc-style.css",RWPC_PLUGIN_URL."/assets/css/rwpc-style.css",array(),RWPC_BUILD);
+    if($hook == 'woocommerce_page_rwpc-remove-woocommerce-product-content') {
+        //STYLES
+        wp_enqueue_style("rwpc-style.css",RWPC_PLUGIN_URL."/assets/css/rwpc-style.css",array(),RWPC_BUILD);
 
-		//SCRIPTS
-		wp_enqueue_script('jquery');	
-		wp_enqueue_script("rwpc-script",RWPC_PLUGIN_URL."/assets/js/rwpc_script.js",array(),RWPC_BUILD);
-	}
+        //SCRIPTS
+        wp_enqueue_script('jquery');	
+        wp_enqueue_script("rwpc-script",RWPC_PLUGIN_URL."/assets/js/rwpc_script.js",array(),RWPC_BUILD);
+    }
 }
 
+/**
+ * Customizes the single product and product category pages in WooCommerce.
+ * Removes specified hooks and actions related to WooCommerce hooks.
+ *
+ * @return void
+ */
 add_action('init', 'rwpc_customizing_single_product_hooks', 2  );
-
 function rwpc_customizing_single_product_hooks() {	
 	if(is_admin())	return;
 
@@ -244,10 +298,15 @@ function rwpc_customizing_single_product_hooks() {
 
 /**
  * Added HPOS support for woocommerce
+ * 
+ * This function is used to declare compatibility with the custom order tables feature in WooCommerce.
+ * It checks if the required class exists and then calls the declare_compatibility method from the FeaturesUtil class.
+ *
+ * @return void
  */
 add_action( 'before_woocommerce_init', 'rwpc_before_woocommerce_init' );
 function rwpc_before_woocommerce_init() {
     if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
-		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
-	}
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+    }
 }
